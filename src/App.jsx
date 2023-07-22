bd40fe27b6b886bd8e590274084e26e812c40adf
sawonhossain34 <sawonhossain3491@gmail.com>
2023-07-22 10:29:35 +0600

    react tailwind daisyui and main layout footer add

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-4xl font-bold'>collegeBD</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
